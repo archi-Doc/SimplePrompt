b8e6794c5649bd030ee0354d5e2d02728ef6fbdb
archi-Doc <archidoc422@gmail.com>
2025-11-27 23:04:51 +0900

    .

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -26,13 +26,13 @@ internal class Program
             ThreadCore.Root.Terminate(); // Send a termination signal to the root.
         };
 
-        /*while (!ThreadCore.Root.IsTerminated)
+        while (!ThreadCore.Root.IsTerminated)
         {
             Console.Write("d");
             Thread.Sleep(100);
             // await Task.Delay(100).ConfigureAwait(false);
             Console.Write("e");
-        }*/
+        }
 
         var builder = new UnitBuilder()
             .Configure(context =>
