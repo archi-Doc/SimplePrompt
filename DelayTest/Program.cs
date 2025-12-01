cf7a22af622ba6c5388ddb94bb5c1aa32a1f4fad
archi-Doc <archidoc422@gmail.com>
2025-12-01 21:33:02 +0900

    .

--- DelayTest/Program.cs
+++ DelayTest/Program.cs
@@ -4,13 +4,34 @@ internal class Program
 {
     public static async Task Main(string[] args)
     {
-        /*while (true)
+        await Log();
+    }
+
+    private static async Task Log()
+    {
+        Console.WriteLine("Log test");
+        while (true)
+        {
+            await Task.Delay(1_000).ConfigureAwait(false);
+
+            var st = $"Cursor {Console.CursorLeft}, {Console.CursorTop} Window {Console.WindowWidth}, {Console.WindowHeight}\n";
+            File.AppendAllText("log.txt", st);
+        }
+    }
+
+    private static async Task TaskDelay()
+    {
+        while (true)
         {
             Console.Write("d");
             await Task.Delay(100).ConfigureAwait(false);
             Console.Write("e");
-        }*/
+        }
+    }
 
+
+    private static async Task ReadLine()
+    {
         Console.Write("> ");
         var st = Console.ReadLine();
         Console.Write(st);
