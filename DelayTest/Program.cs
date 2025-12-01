a9212888111fb0ded141e5c73cb1952d802b767b
archi-Doc <archidoc422@gmail.com>
2025-12-01 20:12:00 +0900

    .

--- DelayTest/Program.cs
+++ DelayTest/Program.cs
@@ -4,11 +4,15 @@ internal class Program
 {
     public static async Task Main(string[] args)
     {
-        while (true)
+        /*while (true)
         {
             Console.Write("d");
             await Task.Delay(100).ConfigureAwait(false);
             Console.Write("e");
-        }
+        }*/
+
+        Console.Write("> ");
+        var st = Console.ReadLine();
+        Console.Write(st);
     }
 }
