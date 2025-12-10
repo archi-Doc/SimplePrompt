9e4b590aa918ee94dfdc1f41c3a466ea344edc9a
archi-Doc <archidoc422@gmail.com>
2025-12-11 00:04:23 +0900

    ...

--- SimplePrompt/Internal/ReadLineRow.cs
+++ SimplePrompt/Internal/ReadLineRow.cs
@@ -1,12 +1,37 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
+using Arc.Collections;
+
 namespace SimplePrompt.Internal;
 
 internal class ReadLineRow
 {
-    public ReadLineBuffer ReadLineBuffer { get; }
+    #region ObjectPool
 
-    public short RowIndex { get; }
+    private const int PoolSize = 16;
+    private static readonly ObjectPool<ReadLineRow> Pool = new(() => new(), PoolSize);
+
+    public static ReadLineRow Rent(ReadLineBuffer readLineBuffer, short rowIndex)
+    {
+        var obj = Pool.Rent();
+        obj.Initialize(readLineBuffer, rowIndex);
+        return obj;
+    }
+
+    public static void Return(ReadLineRow obj)
+    {
+        Pool.Return(obj);
+    }
+
+    #endregion
+
+    #region FiendAndProperty
+
+    public ReadLineBuffer ReadLineBuffer { get; private set; }
+
+    public bool IsMutable { get; private set; }
+
+    public short RowIndex { get; private set; }
 
     public int StartIndex { get; set; }
 
@@ -34,7 +59,14 @@ internal class ReadLineRow
 
     public ReadOnlySpan<byte> WidthSpan => this.ReadLineBuffer.WidthArray.AsSpan(this.StartIndex, this.ImmutableLength + this.MutableLength);
 
-    public ReadLineRow(ReadLineBuffer readLineBuffer, short rowIndex)
+    #endregion
+
+    private ReadLineRow()
+    {
+        this.ReadLineBuffer = default!;
+    }
+
+    private void Initialize(ReadLineBuffer readLineBuffer, short rowIndex)
     {
         this.ReadLineBuffer = readLineBuffer;
         this.RowIndex = rowIndex;
