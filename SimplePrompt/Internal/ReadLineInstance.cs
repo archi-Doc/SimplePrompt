189e854965e2346093c7e692119a5a2a58316e1d
archi-Doc <archidoc422@gmail.com>
2025-12-24 18:20:27 +0900

    .

--- SimplePrompt/Internal/ReadLineInstance.cs
+++ SimplePrompt/Internal/ReadLineInstance.cs
@@ -726,16 +726,16 @@ internal sealed class ReadLineInstance
             return false;
         }*/
 
-        if (!this.RawConsole.TryGetCursorTop(out var newCursorTop))
-        {
-            return false;
-        }
-
-        /*var (_, newCursorTop) = Console.GetCursorPosition(); // I have just got a new theory of eternity in this method, so an interval was added between calls.
-        if (newCursorTop == this.simpleConsole.CursorTop)
+        /*if (!this.RawConsole.TryGetCursorTop(out var newCursorTop))
         {
             return false;
         }*/
+
+        var (_, newCursorTop) = Console.GetCursorPosition(); // I have just got a new theory of eternity in this method, so an interval was added between calls.
+        if (newCursorTop == this.simpleConsole.CursorTop)
+        {
+            return false;
+        }
 
         this.RawConsole.WriteInternal($"<Cursor top {this.simpleConsole.CursorTop} -> {newCursorTop}>"); // coi
 
--- SimplePrompt/SimpleConsole.cs
+++ SimplePrompt/SimpleConsole.cs
@@ -182,13 +182,13 @@ public partial class SimpleConsole : IConsoleService
                     // Active instance: Prepare window and read key input.
                     this.PrepareWindow(currentInstance);
 
-                    if (currentInstance.CorrectCursorTop())
+                    /*if (currentInstance.CorrectCursorTop())
                     {// Since the cursor position has been corrected, redraw the prompt.
                         this.UnderlyingTextWriter.WriteLine();
                         this.NewLineCursor();
                         currentInstance.Redraw();
                         currentInstance.CurrentLocation.Restore(CursorOperation.None);
-                    }
+                    }*/
 
                     if (!this.RawConsole.TryRead(out keyInfo))
                     {
