274329aab3f28ea7e7cc96d25341032ebb291019
archi-Doc <archidoc422@gmail.com>
2025-12-31 14:34:38 +0900

    .

--- SimplePrompt/SimpleConsole.cs
+++ SimplePrompt/SimpleConsole.cs
@@ -130,7 +130,7 @@ public partial class SimpleConsole : IConsoleService
                     Console.WriteLine($"SIGWINCH Height:{Console.WindowHeight} Width:{Console.WindowWidth} Top:{Console.CursorTop}");
                     if (this.instanceList.Count > 0)
                     {
-                        this.AdjustWindow(this.instanceList[^1], false);
+                        this.AdjustWindow(this.instanceList[^1], true);
                     }
                 }
             });
@@ -908,6 +908,7 @@ Exit:
         // Window size changed
         var newCursor = Console.GetCursorPosition();
         this.simpleArrange.Arrange(newCursor, redraw);
+        (this.CursorLeft, this.CursorTop) = newCursor;
     }
 
     internal void ClearRow(int top)
