63fe6e8d7ef9977c58abd25be0947bc978b36c94
archi-Doc <archidoc422@gmail.com>
2025-12-31 14:37:35 +0900

    .

--- SimplePrompt/SimpleConsole.cs
+++ SimplePrompt/SimpleConsole.cs
@@ -127,7 +127,7 @@ public partial class SimpleConsole : IConsoleService
             {
                 using (this.syncObject.EnterScope())
                 {// Adjusts the cursor position when attached to a console.
-                    Console.WriteLine($"SIGWINCH Height:{Console.WindowHeight} Width:{Console.WindowWidth} Top:{Console.CursorTop}");
+                    // Console.WriteLine($"SIGWINCH Height:{Console.WindowHeight} Width:{Console.WindowWidth} Top:{Console.CursorTop}");
                     if (this.instanceList.Count > 0)
                     {
                         this.AdjustWindow(this.instanceList[^1], true);
@@ -908,7 +908,7 @@ Exit:
         // Window size changed
         var newCursor = Console.GetCursorPosition();
         this.simpleArrange.Arrange(newCursor, redraw);
-        (this.CursorLeft, this.CursorTop) = newCursor;
+        // (this.CursorLeft, this.CursorTop) = newCursor;
     }
 
     internal void ClearRow(int top)
