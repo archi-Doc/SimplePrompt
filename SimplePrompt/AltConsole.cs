b3e120b4d3cd6085d19cdf955830ed016a028637
archi-Doc <archidoc422@gmail.com>
2026-04-09 00:20:34 +0900

    .

---
+++ SimplePrompt/AltConsole.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimplePrompt;
+
+public static class AltConsole
+{
+    private static int cursorLeft;
+    private static int cursorTop;
+
+    static AltConsole()
+    {
+    }
+}
