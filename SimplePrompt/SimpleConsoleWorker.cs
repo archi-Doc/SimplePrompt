25c515d66f52b6de831efd0333eaf881a0d4c042
archi-Doc <archidoc422@gmail.com>
2026-04-17 21:21:21 +0900

    .

--- SimplePrompt/SimpleConsoleWorker.cs
+++ SimplePrompt/SimpleConsoleWorker.cs
@@ -6,7 +6,32 @@ using Arc.Threading;
 
 namespace SimplePrompt;
 
-internal sealed class SimpleConsoleWorker : TaskCore
+internal sealed class SimpleConsoleWorker : ThreadCore
+{
+    private readonly SimpleConsole simpleConsole;
+
+    private static void Process(object? parameter)
+    {
+        var worker = (SimpleConsoleWorker)parameter!;
+        while (!worker.simpleConsole.Core.IsTerminated)
+        {
+            worker.simpleConsole.Process();
+
+            Thread.Sleep(10);
+        }
+
+        worker.simpleConsole.Abort();
+    }
+
+    public SimpleConsoleWorker(SimpleConsole simpleConsole, ThreadCoreBase? parent, bool startImmediately = true)
+        : base(parent, Process, startImmediately)
+    {
+        this.simpleConsole = simpleConsole;
+        // this.Thread.IsBackground = true;
+    }
+}
+
+/*internal sealed class SimpleConsoleWorker : TaskCore
 {
     private static readonly TimeSpan IntervalTimeSpan = TimeSpan.FromMilliseconds(10);
 
@@ -28,4 +53,4 @@ internal sealed class SimpleConsoleWorker : TaskCore
     {
         this.simpleConsole = simpleConsole;
     }
-}
+}*/
